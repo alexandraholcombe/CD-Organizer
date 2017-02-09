2ecd561c81027a135c9fb05ab3dadd355705591c
Alexandra Holcombe and Nicole Sanders <alexandra+nicole@>
2017-02-09 10:52:53 -0800

    Add HomeModule.cs

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,28 @@
+using Nancy;
+using CDOrganizer.Objects;
+using System.Collections.Generic;
+
+namespace CDOrganizer
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ => {
+        var allCds = CD.GetAll();
+        return View["all_cds.cshtml", allCds];
+      };
+
+      Get["/new"] = _ => {
+        return View["cd_form.cshtml"];
+      };
+
+      Post["/"] = _ => {
+        CD newCd = new CD(Request.Form["cd-name"]);
+        var allCds = CD.GetAll();
+        return View["all_cds.cshtml", allCds];
+      };
+
+    }
+  }
+}
