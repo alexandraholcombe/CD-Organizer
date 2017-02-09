dd174cd1527eb6447eb8e7e347ee6d4544ae84d2
Alexandra Holcombe and Nicole Sanders <alexandra+nicole@>
2017-02-09 11:34:58 -0800

    Add all_artists.cshtml, modify HomeModule.cs

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -9,19 +9,35 @@ namespace CDOrganizer
     public HomeModule()
     {
       Get["/"] = _ => {
-        var allCds = CD.GetAll();
-        return View["all_cds.cshtml", allCds];
+        var allArtists = Artist.GetAll();
+        return View["all_artists.cshtml", allArtists];
       };
 
-      Get["/new"] = _ => {
-        return View["cd_form.cshtml"];
+      Get["/new-artist"] = _ => {
+        return View["artist_form.cshtml"];
       };
 
       Post["/"] = _ => {
-        var newCd = new CD(Request.Form["cd-name"]);
-        var allCds = CD.GetAll();
-        return View["all_cds.cshtml", allCds];
+        var newArtist = new Artist(Request.Form["artist-name"]);
+        var allArtists = Artist.GetAll();
+        return View["all_artists.cshtml", allArtists];
       };
+
+      // Get["/"] = _ => {
+      //   var allCds = CD.GetAll();
+      //   return View["all_cds.cshtml", allCds];
+      // };
+      //
+      // Get["/new"] = _ => {
+      //   return View["cd_form.cshtml"];
+      // };
+      //
+      // Post["/"] = _ => {
+      //   var newCd = new CD(Request.Form["cd-name"]);
+      //   var allCds = CD.GetAll();
+      //   return View["all_cds.cshtml", allCds];
+      // };
+
 
     }
   }
